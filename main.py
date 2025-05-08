645dd42c10aa507e50ba34a1880764c5fae18d5e
TeslaTech369 <teslatech.me@gmail.com>
2025-05-09 00:35:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from pymongo import MongoClient
 from admin import admin_login, admin_panel
 from student import student_interface, exam_interface, solve_sheet_view
+from routine import routine_view
 from dotenv import load_dotenv
 import os
 
@@ -39,7 +40,7 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 # Sidebar menu
-menu = ["Student", "Admin", "Solve Sheet"]
+menu = ["Student", "Admin", "Solve Sheet", "Routine"]
 choice = st.sidebar.selectbox("Menu", menu)
 
 if choice == "Admin":
@@ -54,4 +55,6 @@ elif choice == "Student":
         student_interface()
 elif choice == "Solve Sheet":
     solve_sheet_view()
+elif choice == "Routine":
+    routine_view()
 
